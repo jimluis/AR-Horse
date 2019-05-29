1f0bb2c8d1fbad6ec5754bd75a858babf3d3527b
Luis Felipe Jimenez <jimluisf@gmail.com>
2019-05-29 14:23:24 -0400

    Adding a script to the buttons to control the animation state

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,14 +5,25 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    public Animator animator;
+    public int currentAnim = 0;
 
     public void Next()
-    { 
-    
+    {
+        if(currentAnim <= 2)
+        {
+            currentAnim++;
+            animator.SetInteger("NextAnimation", currentAnim);
+        }
+
     }
 
     public void Previous()
     {
-
+        if (currentAnim != 0)
+        {
+            currentAnim--;
+            animator.SetInteger("NextAnimation", currentAnim);
+        }
     }
 }
