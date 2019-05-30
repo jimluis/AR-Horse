1c787267285b484d8da592461cdd94a01eebfe7a
Luis Felipe Jimenez <jimluisf@gmail.com>
2019-05-30 10:14:03 -0400

    Adding Calloutlabel script

---
+++ Assets/Scripts/CalloutLabel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalloutLabel : MonoBehaviour
+{
+    [SerializeField] LineRenderer line;
+    [SerializeField] GameObject targetObj;
+
+    void Start()
+    {
+        line.startWidth = 0.01f;
+        line.endWidth = 0.01f;
+        line.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(UIManager.Instance.currentAnim == 1)
+        //{
+        //    UIManager.Instance.ToggleLabel(true);
+        //    line.enabled = true;
+        //    line.SetPosition(0, gameObject.transform.position);
+        //    line.SetPosition(1, targetObj.transform.position);
+        //}
+        //else
+        //{
+        //    line.enabled = false;
+        //    UIManager.Instance.ToggleLabel(false);
+        //}
+
+
+
+    }
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,13 +6,41 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     public Animator animator;
-    public int currentAnim = 0;
+    public int currentAnim;
+
+    public int CurrentAnimGS
+    {
+        get { return currentAnim;  }
+        set 
+        {   
+            currentAnim = value;
+            if (currentAnim == 1)
+                label.SetActive(true);
+            else
+                label.SetActive(false);
+        }
+    }
+
+    private static UIManager instance;
+    public GameObject label;
+
+    public static UIManager Instance
+    {
+        get{ return instance; }
+    }
+
+    private void Awake()
+    {
+        instance = this;
+        label.SetActive(false);
+    }
+
 
     public void Next()
     {
-        if(currentAnim <= 2)
+        if(CurrentAnimGS <= 2)
         {
-            currentAnim++;
+            CurrentAnimGS++;
             animator.SetInteger("NextAnimation", currentAnim);
         }
 
@@ -20,9 +48,9 @@ public class UIManager : MonoBehaviour
 
     public void Previous()
     {
-        if (currentAnim != 0)
+        if (CurrentAnimGS != 0)
         {
-            currentAnim--;
+            CurrentAnimGS--;
             animator.SetInteger("NextAnimation", currentAnim);
         }
     }
